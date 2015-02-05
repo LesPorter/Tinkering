1d46aacfc3b98e9d6f213724405beee60c81ec37
LesPorter <les@lesporter.com>
2015-02-05 00:07:05 +0000

    Added an array and loop to the class example

--- classy.rb
+++ classy.rb
@@ -15,3 +15,26 @@ class Person
   end
   
 end
+
+paula = Person.new("Paula", "Paul")
+puts paula.full_name + " says... "
+paula.say_something
+puts " " # just some blank space between puts
+
+people = []
+people.push Person.new("John", "Doe")
+people.push Person.new("Les", "Porter")
+people.push Person.new("Larry", "McGee")
+
+people[0].say_something
+people[1].say_something
+people[2].say_something
+
+# gee, what if there was an easy way to get
+# all these people to say something, no matter how many?
+puts " " # make some space
+people.each do |person|
+  person.say_something
+  puts "Looping"
+end
+
