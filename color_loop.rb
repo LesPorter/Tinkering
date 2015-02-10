5d5e6b51bd646cfa5e8abd0c3c418d4910fb3271
LesPorter <les@lesporter.com>
2015-02-10 19:50:22 +0000

    Added a comment to color_loop.rb

--- color_loop.rb
+++ color_loop.rb
@@ -5,7 +5,7 @@ user_color = "nothing"
 
 until user_color == "red"
   puts "Sorry."
-  puts "What is your favorite color? hint: red"
+  puts "What is your favorite color? hint: red"  # I tried to these 2 lines in a method but it would have to be a class
   user_color = gets.chomp.to_s
 end
 
